46750b6e03faa967fe38caf3d34d6e1ef44c32e8
meetp-03 <mrlv0103@gmail.com>
2025-03-06 05:41:48 -0500

    Added hook chart
    Hook chart implemented. Labels need adjustment.

--- src/js/script.js
+++ src/js/script.js
@@ -4,15 +4,19 @@
 
 // init global variables, switches, helper functions
 let risingInsight1Chart;
+let hookChart;
 
 function updateAllVisualizations() {
   risingInsight1Chart.wrangleData();
+  hookChart.wrangleData();
+  
 }
 
 // load data using promises
 let promises = [
   d3.csv("data/Shooting_Firearm_Discharges.csv"),
   // If you want to load another dataset just add a new entry here.
+  d3.csv("data/Neighbourhood_Crime_Rates.csv"),
 ];
 
 Promise.all(promises)
@@ -26,10 +30,34 @@ Promise.all(promises)
 // This initializes the main page dynamically via DOM manipulation in D3.js
 function initMainPage(allDataArray) {
   // Add new entry here when you want to destructure other datasets in parallel.
-  const [shootingFireArmsData] = allDataArray;
+  const [shootingFireArmsData, neighbourhoodCrimeData] = allDataArray;
   initRisingInsight1Chart(shootingFireArmsData);
 
   //   risingInsight1Chart = new StackedAreaChart("rising-insight-1-chart");
+  initHookChart(neighbourhoodCrimeData);
+}
+
+function initHookChart(data) {
+  const categories = ["ASSAULT", "ROBBERY", "THEFTOVER", "AUTOTHEFT", "BREAKENTER"];
+  const years = Array.from({ length: 11 }, (_, i) => 2014 + i); // Years from 2014 to 2024
+
+  // Aggregate data for each category and year
+  const aggregatedData = years.map((year) => {
+    const yearData = { year };
+
+    categories.forEach((category) => {
+      const columnName = `${category}_${year}`;
+      yearData[category] = d3.sum(data, (d) => +d[columnName]);
+    });
+
+    return yearData;
+  });
+
+  console.log(aggregatedData);
+
+  const colorScale = d3.scaleOrdinal().domain(["ASSAULT", "ROBBERY", "THEFTOVER", "AUTOTHEFT", "BREAKENTER"]).range(["#3B9EF8", "#3BF848", "#FF4500", "#DFF213", "#EC1BEB"]);
+  // Initialize the Hook chart
+  hookChart = new LineChart("hook-chart", aggregatedData, colorScale, "slider-hook");
 }
 
 // Helper function to offload bulk processing
@@ -71,5 +99,5 @@ function initRisingInsight1Chart(data) {
   // Pass the processed data to the chart
   //   risingInsight1Chart = new StackedAreaChart(`rising-insight-1-chart`, finalData);
   const colorScale = d3.scaleOrdinal().domain(["Morning", "Afternoon", "Evening", "Night"]).range(["#FFD700", "#FF8C00", "#FF4500", "#00008B"]);
-  risingInsight1Chart = new LineChart(`rising-insight-1-chart`, finalData, colorScale);
+  risingInsight1Chart = new LineChart(`rising-insight-1-chart`, finalData, colorScale, "slider");
 }
